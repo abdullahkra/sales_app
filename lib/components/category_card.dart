5c02e51406e765927b373bca89b89f4e4e17d828
abdullahkra <abdullahkaraa13@gmail.com>
2024-03-24 10:27:24 +0300

    +

--- lib/components/category_card.dart
+++ lib/components/category_card.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:sales/models/category.dart';
 
-class CategoryCard extends StatelessWidget {
+class CategoryCard extends StatefulWidget {
   final CategoryModel category;
-  const CategoryCard({
-    super.key,
-    required this.category,
-  });
+  const CategoryCard({super.key, required this.category});
 
+  @override
+  State<CategoryCard> createState() => _CategoryCardState();
+}
+
+class _CategoryCardState extends State<CategoryCard> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        children: [Text(category.name.toString())],
+      elevation: 20,
+      color: Colors.amber,
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(widget.category.name.toString()),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sales/models/category.dart';
+import 'package:sales/models/product.dart';
+import 'package:sales/services/category_service.dart';
+import 'package:sales/services/product_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +12,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<ProductModel> _products = [];
+  List<CategoryModel> _categories=[];
+  void _getProductsData()async{
+    setState(() {
+      
+    });
+    _products = await ProductService.getProductData();
+  }
+  void _getCategoryData()async{
+    setState(() {
+      
+    });
+    _categories = await CategoryService.getCategoryData();
+  }
+  @override
+  void initState() {
+    _getProductsData();
+    _getCategoryData();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
