6ed1d4581224a51c6927a77e7c054e91ed57bd16
abdullahkra <abdullahkaraa13@gmail.com>
2024-03-24 04:26:08 +0300

    up

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+//sdadsa
+void main() {
+  runApp(const sale());
+}
+
+class sale extends StatefulWidget {
+  const sale({super.key});
+
+  @override
+  State<sale> createState() => _saleState();
+}
+
+class _saleState extends State<sale> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
