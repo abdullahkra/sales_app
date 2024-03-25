30c79fdda80fd061237759490194c6ebad2c71ae
abdullahkra <abdullahkaraa13@gmail.com>
2024-03-25 14:30:54 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class Sale extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
